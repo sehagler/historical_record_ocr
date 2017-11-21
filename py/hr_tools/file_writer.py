397d054cde01ff6f5c703b0e009cb61bdfad3304
Stuart Hagler <s.hagler.2015@ieee.org>
2017-11-20 16:15:59 -0800

    Automated correction and segmentation

--- py/hr_tools/file_writer.py
+++ py/hr_tools/file_writer.py
@@ -1,1 +1,30 @@
+#
+import json
+import numpy as np
+from os import makedirs, remove
+from os.path import isfile
+import re
 
+#
+class file_writer(object):
+    
+    #
+    def __init__(self, txt_filename):
+        
+        #
+        self._filename = txt_filename
+        
+        #
+        if isfile(self._filename):
+            remove(self._filename)
+            
+    # append entries to txt-file
+    def _append_entry_to_txt_file(self, entry):
+        with open(self._filename, "a") as myfile:
+            myfile.write(entry)
+    
+    #
+    def run(self, entries):
+        for entry in entries:
+            self._append_entry_to_txt_file(entry)   
+        
