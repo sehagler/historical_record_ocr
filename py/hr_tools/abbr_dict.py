59169c4e9d084f72cb0bab7229742f0697941b17
Stuart Hagler <s.hagler.2015@ieee.org>
2018-01-25 09:55:26 -0800

    full parsing of residential address

--- py/hr_tools/abbr_dict.py
+++ py/hr_tools/abbr_dict.py
@@ -3,15 +3,32 @@ import re
 from xlrd import open_workbook
 
 #
-def _extend_with_errors(xlsx_dict, abbr_list, original_letter, error_letter):
+def _extended_list(text_str):
+    text_str_list = [ text_str ]
+    itr = re.finditer(' ', text_str)
+    idxs = [idx.start() for idx in itr]
+    idxs = sorted(idxs)
+    for i in idxs:
+        text_str_tmp = text_str
+        if i == 0:
+            text_str_tmp = text_str_tmp[1:]
+        elif i == len(text_str_tmp):
+            text_str_tmp = text_str_tmp[:-1]
+        else:
+            text_str_tmp = text_str_tmp[:i] + text_str_tmp[i+1:]
+        text_str_list.append(text_str_tmp)
+    return text_str_list
+
+#
+def _extend_with_errors(upper_flg, xlsx_dict, abbr_list, original_letter, error_letter):
     abbr = abbr_list[0]
     itr = re.finditer(original_letter, abbr)
     idxs = [idx.start() for idx in itr]
     idxs = sorted(idxs)
-    for i in range(len(idxs)):
+    for i in idxs:
         abbr_tmp = abbr
         if i == 0:
-            abbr_tmp = error_letter + abbr_tmp[i+1:]
+            abbr_tmp = error_letter + abbr_tmp[1:]
         elif i == len(abbr):
             abbr_tmp = abbr_tmp[:-1] + error_letter
         else:
@@ -21,71 +38,35 @@ def _extend_with_errors(xlsx_dict, abbr_list, original_letter, error_letter):
     return abbr_list
 
 #
-def _get_specifier_dict():
-    specifier_dict = dict()
-    specifier_dict['bndr'] = 'binder'
-    specifier_dict['dir'] = 'director'
-    specifier_dict['hlpr'] = 'helper'
-    specifier_dict['kpr'] = 'keeper'
-    specifier_dict['mgr'] = 'manager'
-    specifier_dict['mkr'] = 'maker'
-    specifier_dict['mn'] = 'man'
-    specifier_dict['mstr'] = 'master'
-    specifier_dict['opr'] = 'operator'
-    specifier_dict['tndr'] = 'tender'
-    specifier_dict['wkr'] = 'worker'
-    return specifier_dict
-
+def _get_tag_dict(xlsx_file):
+    book = open_workbook(xlsx_file)
+    sheet = book.sheets()[0]
+    tag_dict = {}
+    for i in range(sheet.nrows):
+        rowvals = sheet.row(i)
+        tag_dict[rowvals[0].value] = rowvals[1].value
+    return tag_dict
+    
 #
-def _unpack_occupation_key(upper_flg, xlsx_dict, rowval0, rowval1, key_str, value_str): 
-    if upper_flg:
-        key_str = key_str.upper()
-        value_str = value_str.upper()
-    if key_str == 'mn':
-        len_key_str = len(key_str) + 1
-        len_value_str = len(value_str)
-        if len(rowval0) > len_key_str and rowval0[-len_key_str:] == ' ' + key_str:
-            key_base = rowval0[:-len_key_str]
-            value_base = rowval1[:-len_value_str]
-            xlsx_dict = _xlsx_dict_append(xlsx_dict, key_base + ' man', value_base + 'man')
-            xlsx_dict = _xlsx_dict_append(xlsx_dict, key_base + 'man', value_base + 'man')
-            xlsx_dict = _xlsx_dict_append(xlsx_dict, key_base + 'mn', value_base + 'man')
-            xlsx_dict = _xlsx_dict_append(xlsx_dict, key_base + ' wm', value_base + 'woman')
-            xlsx_dict = _xlsx_dict_append(xlsx_dict, key_base + ' wn', value_base + 'woman')
-            xlsx_dict = _xlsx_dict_append(xlsx_dict, key_base + 'wm', value_base + 'woman')
-            xlsx_dict = _xlsx_dict_append(xlsx_dict, key_base + 'wn', value_base + 'woman')
-    else:
-        len_key_str = len(key_str) + 1
-        len_value_str = len(value_str)
-        if len(rowval0) > len_key_str and rowval0[-len_key_str:] == ' ' + key_str:
-            key_base = rowval0[:-len_key_str]
-            value_base = rowval1[:-len_value_str]
-            xlsx_dict = _xlsx_dict_append(xlsx_dict, key_base + key_str, value_base + value_str)
+def _xlsx_dict_append_with_common_ocr_errors(upper_flg, xlsx_dict, abbr, value):
+    abbr_list = [ abbr ]
+    abbr_list = _extend_with_errors(upper_flg, xlsx_dict, abbr_list, ' ', '')
+    abbr_list = _extend_with_errors(upper_flg, xlsx_dict, abbr_list, 'c', 'e')
+    abbr_list = _extend_with_errors(upper_flg, xlsx_dict, abbr_list, 'c', 'C')
+    abbr_list = _extend_with_errors(upper_flg, xlsx_dict, abbr_list, 'd', 'a')
+    abbr_list = _extend_with_errors(upper_flg, xlsx_dict, abbr_list, 'l', 'i')
+    abbr_list = _extend_with_errors(upper_flg, xlsx_dict, abbr_list, 'l', '1')
+    abbr_list = _extend_with_errors(upper_flg, xlsx_dict, abbr_list, 'n', 'h')
+    abbr_list = _extend_with_errors(upper_flg, xlsx_dict, abbr_list, 'n', 'r')
+    abbr_list = _extend_with_errors(upper_flg, xlsx_dict, abbr_list, 'o', 'Q')
+    for abbr_tmp in abbr_list:
+        xlsx_dict[' ' + abbr_tmp + ' '] = ' ' + value + ' '
     return xlsx_dict
 
 #
-def _unpack_occupation_value(rowval_list, rowval, value_str):
-    if value_str == 'man':
-        len_value_str = len(value_str) + 1
-        if len(rowval) > len_value_str and rowval[-len_value_str:] == ' ' + value_str:
-            rowval_list.append(rowval[:-len_value_str] + value_str)
-            rowval_list.append(rowval[:-len_value_str] + ' woman')
-            rowval_list.append(rowval[:-len_value_str] + 'woman')
-    else:
-        len_value_str = len(value_str) + 1
-        if len(rowval) > len_value_str and rowval[-len_value_str:] == ' ' + value_str:
-            rowval_list.append(rowval[:-len_value_str] + value_str)
-    return rowval_list
-    
-#
-def _xlsx_dict_append(xlsx_dict, abbr, value):
-    abbr_list = [ abbr ]
-    abbr_list = _extend_with_errors(xlsx_dict, abbr_list, ' ', '')
-    abbr_list = _extend_with_errors(xlsx_dict, abbr_list, 'c', 'e')
-    abbr_list = _extend_with_errors(xlsx_dict, abbr_list, 'd', 'a')
-    abbr_list = _extend_with_errors(xlsx_dict, abbr_list, 'l', 'i')
-    for abbr_tmp in abbr_list:
-        xlsx_dict[' ' + abbr_tmp + ' '] = ' ' + value + ' '
+def _xlsx_dict_append(upper_flg, xlsx_dict, abbr, value):
+    xlsx_dict = _xlsx_dict_append_with_common_ocr_errors(upper_flg, xlsx_dict, abbr, value)
+    xlsx_dict = _xlsx_dict_append_with_common_ocr_errors(upper_flg, xlsx_dict, abbr.lower(), value)
     return xlsx_dict
 
 #
@@ -104,17 +85,17 @@ def abbr_dict(upper_flg, mode_flg, xlsx_file):
                 rowval.append(rowval_tmp)
         if mode_flg == 1:
             if len(rowval[0]) == 1:
-                xlsx_dict = _xlsx_dict_append(xlsx_dict, rowval[0], rowval[1])
+                xlsx_dict = _xlsx_dict_append_with_common_ocr_errors(upper_flg, xlsx_dict, rowval[0], rowval[1])
         elif mode_flg == 2:
             if len(rowval[0]) >= 2:
-                xlsx_dict = _xlsx_dict_append(xlsx_dict, rowval[0], rowval[1])
+                xlsx_dict = _xlsx_dict_append_with_common_ocr_errors(upper_flg, xlsx_dict, rowval[0], rowval[1])
     return xlsx_dict
 
 #
-def abbr_dict_occupations(upper_flg, mode_flg, xlsx_file):
-    specifier_dict = _get_specifier_dict()
-    key_list = list(specifier_dict.keys())
-    book = open_workbook(xlsx_file)
+def abbr_dict_occupations(upper_flg, mode_flg, xlsx_file1, xlsx_file2):
+    tag_dict = _get_tag_dict(xlsx_file2)
+    tag_key_list = list(tag_dict.keys())
+    book = open_workbook(xlsx_file1)
     sheet = book.sheets()[0]
     xlsx_dict = {}
     for i in range(sheet.nrows):
@@ -126,44 +107,95 @@ def abbr_dict_occupations(upper_flg, mode_flg, xlsx_file):
                 rowval.append(rowval_tmp.upper())
             else:
                 rowval.append(rowval_tmp)
-        if rowval[0] != rowval[1]:
-            if mode_flg == 1:
-                if len(rowval[0]) == 1:
-                    xlsx_dict = _xlsx_dict_append(xlsx_dict, rowval[0], rowval[1])
-            elif mode_flg == 2:
+        if rowval[1][-3:] == 'XYZ':
+            if mode_flg == 2:
                 if len(rowval[0]) >= 2:
-                    xlsx_dict = _xlsx_dict_append(xlsx_dict, rowval[0], rowval[1])
-                    for key in key_list:
-                        xlsx_dict = _unpack_occupation_key(upper_flg, xlsx_dict, rowval[0], rowval[1],
-                                                           key, specifier_dict[key])
+                    rowval1_base = rowval[1]
+                    rowval0_base = rowval[0]
+                    if rowval0_base[:-4] == rowval1_base[:-4]:
+                        for tag_key in tag_key_list:
+                            rowval0 = rowval0_base[:-4] + tag_key
+                            rowval1 = rowval1_base[:-3] + tag_dict[tag_key]
+                            xlsx_dict = _xlsx_dict_append(upper_flg, xlsx_dict, rowval0, rowval1) 
+                    else:
+                        rowval0 = rowval0_base[:-4]
+                        rowval1 = rowval1_base[:-4]
+                        xlsx_dict = _xlsx_dict_append(upper_flg, xlsx_dict, rowval0, rowval1)
+                        for tag_key in tag_key_list:
+                            rowval0 = rowval0_base[:-4] + tag_key
+                            rowval1 = rowval1_base[:-3] + tag_dict[tag_key]
+                            xlsx_dict = _xlsx_dict_append(upper_flg, xlsx_dict, rowval0, rowval1)
+                            rowval0 = rowval1_base[:-4] + tag_key
+                            rowval1 = rowval1_base[:-3] + tag_dict[tag_key]
+                            xlsx_dict = _xlsx_dict_append(upper_flg, xlsx_dict, rowval0, rowval1) 
+        elif rowval[0] != rowval[1]:
+            xlsx_dict = _xlsx_dict_append(upper_flg, xlsx_dict, rowval[0], rowval[1]) 
     return xlsx_dict
 
 #
-def dict_correction(dictionary, text_str):
+def dict_correction(spurious_initial_flg, spurious_final_flg, dictionary, text_str):
     key_list = sorted(list(dictionary.keys()), key = len, reverse = True)
     for key in key_list:
         match = re.search(key, text_str)
         if match is not None:
             text_str = re.sub(key, dictionary[key], text_str)
-    return text_str
+        if spurious_initial_flg:
+            match = re.search(r' [A-Z]' + key[1:], text_str)
+            if match is not None:
+                text_str = re.sub(r' [A-Z]' + key[1:], 
+                                  ' ' + text_str[match.start()+1] + ' ' + dictionary[key], text_str)
+            if spurious_final_flg:
+                match = re.search(key[:-1] + r'[A-Z] ', text_str)
+                if match is not None:
+                    text_str = re.sub(key[:-1] + r'[A-Z] ', 
+                                      dictionary[key] + ' ' + text_str[match.start() + len(key) - 1] + ' ', 
+                                      text_str)
+    return text_str       
 
 #
-def get_occupation_list(xlsx_file):
-    specifier_dict = _get_specifier_dict()
-    key_list = list(specifier_dict.keys())
+def get_value_list(xlsx_file):
     book = open_workbook(xlsx_file)
     sheet = book.sheets()[0]
-    occupation_list = []
+    value_list = []
     for i in range(sheet.nrows):
         rowvals = sheet.row(i)
         rowval = rowvals[1].value
-        rowval_list = [ rowval ]
-        for key in key_list:
-            rowval_list = _unpack_occupation_value(rowval_list, rowval, specifier_dict[key])
+        value_list.append(rowval.upper())
+    value_list = list(set(value_list))
+    value_list.sort(key = lambda x:len(x), reverse = True)
+    return value_list
+
+#
+def get_value_list_occupations(occupations_flg, xlsx_file1, xlsx_file2):
+    tag_dict = _get_tag_dict(xlsx_file2)
+    tag_key_list = list(tag_dict.keys())
+    book = open_workbook(xlsx_file1)
+    sheet = book.sheets()[0]
+    value_list = []
+    for i in range(sheet.nrows):
+        rowvals = sheet.row(i)
+        rowval = rowvals[1].value
+        rowval_list = []
+        if rowval[-3:] == 'XYZ':
+            for tag_key in tag_key_list:
+                extended_rowval = _extended_list(rowval[:-3] + tag_dict[tag_key])
+                rowval_list = list(set(rowval_list) | set(extended_rowval))
+        else:
+            extended_rowval = _extended_list(rowval)
+            rowval_list = list(set(rowval_list) | set(extended_rowval))
         for rowval in rowval_list:
             if rowval != '':
-                occupation_list.append(rowval.upper())
-                occupation_list.append('HEAD ' + rowval.upper())
-    occupation_list = list(set(occupation_list))
-    occupation_list.sort(key = lambda x:len(x), reverse = True)
-    return occupation_list
+                value_list.append(rowval.upper())
+                if occupations_flg:
+                    value_list.append('ASSISTANT ' + rowval.upper())
+                    value_list.append('CHIEF ' + rowval.upper())
+                    value_list.append('HEAD ' + rowval.upper())
+    value_list = list(set(value_list))
+    value_list.sort(key = lambda x:len(x), reverse = True)
+    return value_list
+
+#
+def get_key_list(xlsx_file):
+    xlsx_dict = abbr_dict('False', 2, xlsx_file)
+    key_list = list(xlsx_dict.keys())
+    return key_list
